1c2e98a2a3ccdfea45fccc5a1ea5b5db9d86f141
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:17 +0100

    improve

--- components/Cursors/index.ts
+++ components/Cursors/index.ts
@@ -1,1 +1,8 @@
-export * from "./Cursors";
+/**
+ * This module exports all the cursors-related functionality.
+ */
+
+// Re-export all the cursors-related functionality from the Cursors module.
+module.exports = {
+  ...require('./Cursors'),
+};
