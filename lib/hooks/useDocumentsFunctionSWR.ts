5113c9a2caf815baa5ecb1324faffbac14d9563b
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:32 +0100

    improve

--- lib/hooks/useDocumentsFunctionSWR.ts
+++ lib/hooks/useDocumentsFunctionSWR.ts
@@ -4,19 +4,27 @@ import useSWR, { SWRConfiguration } from "swr";
  * Custom `useSWR` hook that takes server actions from `/lib/actions`
  *
  * @example
- * const { data, error, mutate } = useDocumentsFunctionSWR([
- *   getDocumentUsers, { documentId }
- * ],{ refreshInterval: 0 });
+ * const { data, error, mutate } = useDocumentsFunctionSWR(
+ *   getDocumentUsers,
+ *   { documentId },
+ *   { refreshInterval: 0 }
+ * );
  *
- * @param documentActionAndArgs - A tuple containing the server action, and the argument to pass
- * @param swrOptions - SWR configuration
+ * @template T The type of the server action function
+ * @template R The type of the return value of the server action function
+ * @param documentAction A server action function
+ * @param documentActionArgs The arguments to pass to the server action function
+ * @param swrOptions SWR configuration
  */
-export function useDocumentsFunctionSWR<T extends (...args: any) => any>(
-  documentActionAndArgs: [T | null, Parameters<T> | Parameters<T>[0]],
+export function useDocumentsFunctionSWR<T extends (...args: any) => Promise<{ data: any }>, R = Awaited<ReturnType<T>>["data"]>(
+  documentAction: T,
+  documentActionArgs: Parameters<T>,
   swrOptions: SWRConfiguration = {}
 ) {
-  const fetcher = async ([func, ...args]: [T, Parameters<T>[]]) => {
-    const { data, error } = await func(...args);
+  const fetcher = async (...args: [...Parameters<T>, SWRConfiguration?]): Promise<R> => {
+    const [action, ...actionArgs] = args;
+
+    const { data, error } = await action(...actionArgs);
 
     if (error) {
       console.error(error.message);
@@ -26,9 +34,5 @@ export function useDocumentsFunctionSWR<T extends (...args: any) => any>(
     return data;
   };
 
-  return useSWR<Awaited<ReturnType<T>>["data"]>(
-    [...documentActionAndArgs],
-    fetcher,
-    swrOptions
-  );
+  return useSWR<R>([documentAction, ...documentActionArgs], fetcher, swrOptions);
 }
