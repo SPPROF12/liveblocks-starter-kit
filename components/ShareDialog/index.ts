2bf1979955b84bdcbb0bd66b8630f94d515db286
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:24 +0100

    improve

--- components/ShareDialog/index.ts
+++ components/ShareDialog/index.ts
@@ -1,6 +1,12 @@
-export * from "./ShareDialog";
-export * from "./ShareDialogDefault";
-export * from "./ShareDialogGroups";
-export * from "./ShareDialogInviteGroup";
-export * from "./ShareDialogInviteUser";
-export * from "./ShareDialogUsers";
+export * as ShareDialog from "./ShareDialog";
+export * as ShareDialogDefault from "./ShareDialogDefault";
+export * as ShareDialogGroups from "./ShareDialogGroups";
+export * as ShareDialogInviteGroup from "./ShareDialogInviteGroup";
+export * as ShareDialogInviteUser from "./ShareDialogInviteUser";
+export * as ShareDialogUsers from "./ShareDialogUsers";
+
+
+import * as ShareDialog from "path/to/share-dialog";
+
+
+import { ShareDialog } from "path/to/share-dialog";
