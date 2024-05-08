81bc4d11b5ca02634f6e0fb3f1fe8a958da9d8ac
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:38 +0100

    improve

--- components/TextEditor/CustomTaskItem.tsx
+++ components/TextEditor/CustomTaskItem.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef } from "react";
 import TaskItem from "@tiptap/extension-task-item";
 import {
   NodeViewContent,
@@ -5,27 +6,39 @@ import {
   NodeViewWrapper,
   ReactNodeViewRenderer,
 } from "@tiptap/react";
-import { Checkbox } from "@/primitives/Checkbox";
+import Checkbox from "@/primitives/Checkbox";
 import styles from "./CustomTaskItem.module.css";
 
+type CustomTaskItemProps = NodeViewProps & {
+  node: TaskItem;
+};
+
 // A custom task item that uses the checkbox primitive
-export const CustomTaskItem = TaskItem.extend({
+const CustomTaskItem = TaskItem.extend({
   addNodeView: () => {
     return ReactNodeViewRenderer(TiptapCheckbox);
   },
 });
 
-function TiptapCheckbox({ node, updateAttributes }: NodeViewProps) {
-  return (
-    <NodeViewWrapper className={styles.tiptapTaskItem}>
-      <label className={styles.tiptipTaskItemCheckbox} contentEditable={false}>
-        <Checkbox
-          initialValue={false}
-          checked={node.attrs.checked}
-          onValueChange={(checked: boolean) => updateAttributes({ checked })}
-        />
-      </label>
-      <NodeViewContent className={styles.tiptipTaskItemContent} />
-    </NodeViewWrapper>
-  );
-}
+const TiptapCheckbox = forwardRef<HTMLDivElement, CustomTaskItemProps>(
+  ({ node, updateAttributes, className }, ref) => {
+    return (
+      <NodeViewWrapper
+        className={className}
+        elementRef={ref}
+        key={node.key}
+      >
+        <label className={styles.tiptapTaskItemCheckbox} contentEditable={false}>
+          <Checkbox
+            initialValue={false}
+            checked={node.attrs.checked}
+            onValueChange={(checked: boolean) => updateAttributes({ checked })}
+          />
+        </label>
+        <NodeViewContent className={styles.tiptipTaskItemContent} />
+      </NodeViewWrapper>
+    );
+  }
+);
+
+export default CustomTaskItem;
