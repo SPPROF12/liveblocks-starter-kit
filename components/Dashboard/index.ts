d0ddba9c9499c7d9490801515e5534b5bbdb1cc9
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:30 +0100

    improve

--- components/Dashboard/index.ts
+++ components/Dashboard/index.ts
@@ -1,2 +1,16 @@
-export * from "./DashboardSidebar";
-export * from "./DashboardHeader";
+import * as DashboardSidebar from "./DashboardSidebar";
+import * as DashboardHeader from "./DashboardHeader";
+
+const dashboardExports = {
+  ...DashboardSidebar,
+  ...DashboardHeader,
+};
+
+export default dashboardExports;
+
+
+import dashboardExports from "./dashboardExports";
+
+// Use the named exports like this:
+dashboardExports.DashboardSidebar.someNamedExport();
+dashboardExports.DashboardHeader.anotherNamedExport();
