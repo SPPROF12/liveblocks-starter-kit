68737e89ba60b781f50cba4b8ad2410f95520ca9
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:29 +0100

    improve

--- components/Document/index.ts
+++ components/Document/index.ts
@@ -1,1 +1,10 @@
-export * from "./DocumentHeader";
+// re-export all exports from DocumentHeader module
+export * as DocumentHeader from "./DocumentHeader";
+
+// re-export specific exports from DocumentHeader module using alias
+export { default as DocumentHeaderSummary } from "./DocumentHeader/DocumentHeaderSummary";
+export { default as DocumentHeaderDetails } from "./DocumentHeader/DocumentHeaderDetails";
+
+// re-export specific exports from DocumentHeader module without alias
+export { DocumentHeaderConstants } from "./DocumentHeader/DocumentHeaderConstants";
+
