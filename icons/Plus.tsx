ed23fb3182afc90a282fef5357af00e48ee9d5fb
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:20 +0100

    improve

--- icons/Plus.tsx
+++ icons/Plus.tsx
@@ -1,10 +1,10 @@
-import { ComponentProps } from "react";
+import { SVGProps } from "react";
 
-export function PlusIcon(props: ComponentProps<"svg">) {
+export function PlusIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="16"
-      height="16"
+      width={16}
+      height={16}
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
