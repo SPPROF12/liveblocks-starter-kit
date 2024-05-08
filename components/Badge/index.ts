04d7cd1bcc86db21a6e756217f62a993ddf8cea6
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:16 +0100

    improve

--- components/Badge/index.ts
+++ components/Badge/index.ts
@@ -1,1 +1,9 @@
-export * from "./Badge";
+// re-export individual named exports from "./Badge" module
+export { default as Badge } from "./Badge";
+export { BadgeSizes } from "./Badge";
+export { BadgeColors } from "./Badge";
+
+// or if you want to re-export all named exports from "./Badge" module
+// you can use the following line instead
+// import * as BadgeModule from "./Badge";
+// export * as BadgeModule from "./Badge";
