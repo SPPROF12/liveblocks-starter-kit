143c3dc1079b0c69e6b5230a8a39a04eff274c00
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:10 +0100

    improve

--- primitives/Avatar/Avatar.tsx
+++ primitives/Avatar/Avatar.tsx
@@ -1,113 +1,136 @@
 import clsx from "clsx";
 import Image from "next/image";
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
+import { Tooltip, TooltipProps, tooltipVariants } from "../Tooltip";
 import { getInitials } from "../../utils";
-import { Tooltip } from "../Tooltip";
-import styles from "./Avatar.module.css";
+import { avatarVariants, avatarClassName } from "./Avatar.module.css";
 
 const DEFAULT_SIZE = 24;
 const FONT_SIZE_FACTOR = 0.36;
 
-export interface Props extends Omit<ComponentProps<"div">, "color"> {
+export interface AvatarProps extends Omit<ComponentProps<"div">, "color"> {
   src?: string;
   name: string;
   size?: number;
   color?: string;
   outline?: boolean;
   tooltip?: boolean;
-  tooltipProps?: Omit<ComponentProps<typeof Tooltip>, "children" | "content">;
+  tooltipProps?: Omit<TooltipProps, "children" | "content">;
+  variants?: string;
+  className?: string;
+  sx?: ComponentProps<"div">["style"];
+  ref?: React.Ref<HTMLDivElement>;
 }
 
-interface EllipsisProps extends ComponentProps<"div"> {
+export interface AvatarEllipsisProps
+  extends Omit<ComponentProps<"div">, "color" | "ellipsis"> {
   ellipsis: number;
   size?: number;
   outline?: boolean;
   tooltip?: boolean;
-  tooltipProps?: Omit<ComponentProps<typeof Tooltip>, "children" | "content">;
+  tooltipProps?: Omit<TooltipProps, "children" | "content">;
+  color?: string;
+  title?: string;
+  variants?: string;
+  className?: string;
+  sx?: ComponentProps<"div">["style"];
+  ref?: React.Ref<HTMLDivElement>;
 }
 
-export function Avatar({
-  src,
-  size = DEFAULT_SIZE,
-  outline = false,
-  name,
-  color,
-  tooltip = false,
-  tooltipProps,
-  className,
-  style,
-  ...props
-}: Props) {
-  const content = (
-    <div
-      className={clsx(
-        styles.avatar,
-        className,
-        outline && styles.avatarOutline
-      )}
-      style={{ width: size, height: size, color, ...style }}
-      aria-label={name}
-      {...props}
-    >
-      {src && (
-        <Image alt={name} src={src} height={size} width={size} aria-hidden />
-      )}
-      <span
-        style={{ fontSize: size * FONT_SIZE_FACTOR }}
-        className={styles.label}
-        aria-hidden
+export const Avatar = forwardRef<HTMLDivElement, AvatarProps>(
+  ({ src, name, size = DEFAULT_SIZE, color, outline, tooltip, tooltipProps, variants, className, sx, ...props }, ref) => {
+    const content = (
+      <div
+        ref={ref}
+        className={clsx(
+          avatarClassName.base,
+          outline && avatarClassName.outline,
+          variants,
+          className
+        )}
+        style={{ width: size, height: size, color, ...sx, ...props.style }}
+        aria-label={name}
       >
-        {getInitials(name)}
-      </span>
-    </div>
-  );
+        {src && (
+          <Image
+            alt={name}
+            src={src}
+            height={size}
+            width={size}
+            aria-hidden
+          />
+        )}
+        <span
+          style={{ fontSize: size * FONT_SIZE_FACTOR }}
+          className={avatarClassName.label}
+          aria-hidden
+        >
+          {getInitials(name)}
+        </span>
+      </div>
+    );
 
-  return tooltip ? (
-    <Tooltip content={name} {...tooltipProps}>
-      {content}
-    </Tooltip>
-  ) : (
-    content
-  );
-}
-
-export function AvatarEllipsis({
-  ellipsis,
-  size = DEFAULT_SIZE,
-  outline = false,
-  tooltip = false,
-  tooltipProps,
-  className,
-  style,
-  ...props
-}: EllipsisProps) {
-  const content = (
-    <div
-      className={clsx(
-        styles.avatar,
-        className,
-        outline && styles.avatarOutline
-      )}
-      style={{ width: size, height: size, ...style }}
-      {...props}
-    >
-      <span
-        style={{ fontSize: size * FONT_SIZE_FACTOR }}
-        className={styles.label}
+    return tooltip ? (
+      <Tooltip
+        content={name}
+        {...tooltipProps}
+        variants={tooltipVariants}
+        ref={ref}
       >
-        +{ellipsis}
-      </span>
-    </div>
-  );
+        {content}
+      </Tooltip>
+    ) : (
+      content
+    );
+  }
+);
 
-  return tooltip ? (
-    <Tooltip
-      content={`${ellipsis} other${ellipsis > 1 ? "s" : ""}`}
-      {...tooltipProps}
-    >
-      {content}
-    </Tooltip>
-  ) : (
-    content
-  );
-}
+export const AvatarEllipsis = forwardRef<HTMLDivElement, AvatarEllipsisProps>(
+  ({
+    ellipsis,
+    size = DEFAULT_SIZE,
+    outline = false,
+    tooltip = false,
+    tooltipProps,
+    color,
+    title,
+    variants,
+    className,
+    sx,
+    ...props
+  }, ref) => {
+    const content = (
+      <div
+        ref={ref}
+        className={clsx(
+          avatarClassName.base,
+          outline && avatarClassName.outline,
+          variants,
+          className
+        )}
+        style={{ width: size, height: size, color, ...sx, ...props.style }}
+        title={title}
+      >
+        <span
+          style={{ fontSize: size * FONT_SIZE_FACTOR }}
+          className={avatarClassName.label}
+        >
+          +{ellipsis}
+        </span>
+      </div>
+    );
+
+    return tooltip ? (
+      <Tooltip
+        content={`${ellipsis} other${ellipsis > 1 ? "s" : ""}`}
+        {...tooltipProps}
+        variants={tooltipVariants}
+        ref={ref}
+      >
+        {content}
+      </Tooltip>
+    ) : (
+      content
+    );
+  }
+);
