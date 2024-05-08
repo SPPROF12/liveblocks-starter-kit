e9ed82c63a0ff07043096551053e3932f059f4e5
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:41 +0100

    improve

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,19 @@
-export * from "./capitalize";
-export * from "./getContrastingColor";
-export * from "./getInitials";
-export * from "./useBoundingClientRectRef";
-export * from "./normalizeTrailingSlash";
+export * from './capitalize';
+export * from './getContrastingColor';
+export * from './getInitials';
+export * from './useBoundingClientRectRef';
+export * from './normalizeTrailingSlash';
+
+// can be rewritten as
+
+const moduleNames = [
+  './capitalize',
+  './getContrastingColor',
+  './getInitials',
+  './useBoundingClientRectRef',
+  './normalizeTrailingSlash'
+];
+
+moduleNames.forEach(moduleName => {
+  export * from moduleName;
+});
