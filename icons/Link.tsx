d293875857a3ed4280f82d07070d1303791b0511
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:27 +0100

    improve

--- icons/Link.tsx
+++ icons/Link.tsx
@@ -1,30 +1,30 @@
-import { ComponentProps } from "react";
+import { ComponentProps, SVGProps } from "react";
 
-export function LinkIcon(props: ComponentProps<"svg">) {
+const cssProps = {
+  width: "16",
+  height: "16",
+  viewBox: "0 0 16 16",
+  xmlns: "http://www.w3.org/2000/svg",
+  strokeWidth: "2",
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
+  vectorEffect: "non-scaling-stroke",
+};
+
+export function LinkIcon(
+  props: ComponentProps<"svg"> & { className?: string }
+) {
   return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
+    <svg {...cssProps} {...props}>
       <path
-        d="M7 8.955a3.034 3.034 0 0 0 .942.861 2.792 2.792 0 0 0 2.43.184 2.918 2.918 0 0 0 1.047-.71l1.758-2.05c.534-.584.83-1.365.823-2.175a3.178 3.178 0 0 0-.858-2.16A2.856 2.856 0 0 0 11.095 2a2.85 2.85 0 0 0-2.061.868l-.504.528"
+        d="M7 9a3 3 0 0 0 .943.86A2.8 2.8 0 0 0 9 11a2.8 2.8 0 0 0 2.061-.86l1.758-2.05c.534-.584.83-1.365.823-2.175a3.2 3.2 0 0 0-1.858-2.66A3 3 0 0 0 7 4a3 3 0 0 0-1.858.34Z"
         stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        vectorEffect="non-scaling-stroke"
+        fill="none"
       />
       <path
-        d="M9 7.045a2.983 2.983 0 0 0-.942-.861A2.79 2.79 0 0 0 5.628 6a2.918 2.918 0 0 0-1.047.71L2.823 8.76A3.184 3.184 0 0 0 2 10.936c.007.811.315 1.586.858 2.16A2.856 2.856 0 0 0 4.905 14a2.851 2.851 0 0 0 2.061-.868l.501-.528"
+        d="M9 7a2.938 2.938 0 0 0-.943-.86A2.763 2.763 0 0 0 5.628 6a2.938 2.938 0 0 0-1.047.71L2.823 8.76a3.184 3.184 0 0 0-.17.94A2.856 2.856 0 0 0 4.905 14a2.856 2.856 0 0 0 2.061-.868l.501-.528Z"
         stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        vectorEffect="non-scaling-stroke"
+        fill="none"
       />
     </svg>
   );
