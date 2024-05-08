f528241f7196764b976ce4f8bc2cb9b19b2309ee
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:27 +0100

    improve

--- utils/getContrastingColor.ts
+++ utils/getContrastingColor.ts
@@ -1,53 +1,65 @@
-export function getContrastingColor(col: string) {
-  if (typeof window === "undefined") {
+import { RGB } from './types';
+
+export function getContrastingColor(col: string): string | undefined {
+  if (typeof window === 'undefined') {
     return;
   }
-  const useBlack = getColor(hexToRgb(standardizeColor(col)));
-  return useBlack ? "#000000" : "#ffffff";
-}
 
-type RGB = {
-  r: number;
-  g: number;
-  b: number;
-} | null;
-
-function getColor(rgb: RGB) {
+  const rgb = hexToRgb(standardizeColor(col));
   if (!rgb) {
     return;
   }
 
+  return getColor(rgb) ? '#000000' : '#ffffff';
+}
+
+type HexColor = `#${string}`;
+
+type RGBObject = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+function standardizeColor(str: string): HexColor {
+  if (str.startsWith('#')) {
+    return str;
+  }
+
+  return `#${str}`;
+}
+
+function hexToRgb(hex: HexColor): RGBObject | null {
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = standardizeColor(hex);
+
+  const result = shorthandRegex.exec(hex);
+  if (result) {
+    return {
+      r: parseInt(result[1].repeat(2), 16),
+      g: parseInt(result[2].repeat(2), 16),
+      b: parseInt(result[3].repeat(2), 16),
+    };
+  }
+
+  const longhandRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+  const longhandResult = longhandRegex.exec(hex);
+  if (longhandResult) {
+    return {
+      r: parseInt(longhandResult[1], 16),
+      g: parseInt(longhandResult[2], 16),
+      b: parseInt(longhandResult[3], 16),
+    };
+  }
+
+  return null;
+}
+
+function getColor(rgb: RGB): boolean {
   const { r, g, b } = rgb;
   if (r && g && b) {
-    const isLight = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-    return isLight < 0.5;
+    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return luminance > 160;
   }
   return false;
-}
-
-function standardizeColor(str: string): string {
-  const ctx = document.createElement("canvas").getContext("2d");
-  if (!ctx) {
-    return "";
-  }
-
-  ctx.fillStyle = str;
-  return ctx.fillStyle;
-}
-
-function hexToRgb(hex: string): RGB {
-  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-    return r + r + g + g + b + b;
-  });
-
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
 }
