13b92380ed4c5f90ef6b1f52efebd5de09692ee7
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:44 +0100

    improve

--- icons/AlignRight.tsx
+++ icons/AlignRight.tsx
@@ -1,6 +1,10 @@
-import { ComponentProps } from "react";
+import { ComponentProps, SVGProps } from "react";
 
-export function AlignRightIcon(props: ComponentProps<"svg">) {
+export function AlignRightIcon(
+  props: ComponentProps<"svg"> & { cssProps?: SVGProps<SVGSVGElement> }
+) {
+  const { cssProps, ...svgProps } = props;
+
   return (
     <svg
       width="20"
@@ -8,11 +12,13 @@ export function AlignRightIcon(props: ComponentProps<"svg">) {
       viewBox="0 0 16 16"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      {...svgProps}
+      {...cssProps}
+      aria-hidden
     >
       <path
+        d="M12.5 2H7a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM7 4h5v1H7V4zm0 2h5v1H7V6zm0 2h5v1H7V8zm0 2h5v1H7v-1zm0 2h5v1H7v-1z"
         fillRule="evenodd"
-        d="M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
       />
     </svg>
   );
