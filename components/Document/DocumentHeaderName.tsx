64c07fd47fe55dda422d865dd31c19449abe4de5
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:23 +0100

    improve

--- components/Document/DocumentHeaderName.tsx
+++ components/Document/DocumentHeaderName.tsx
@@ -9,45 +9,57 @@ import {
 import { EditIcon } from "@/icons";
 import { useInitialDocument } from "@/lib/hooks/useInitialDocument";
 import { useSelf } from "@/liveblocks.config";
-import { Tooltip } from "@/primitives/Tooltip";
+import { Tooltip, TooltipProps } from "@/primitives/Tooltip";
 import styles from "./DocumentHeaderName.module.css";
 
-interface Props extends ComponentProps<"div"> {
+type DocumentRenameEventHandler = () => void;
+type DocumentRenameCancelEventHandler = () => void;
+type DocumentRenameSaveEventHandler = () => void;
+type DocumentNameChangeEventHandler = (event: ChangeEvent<HTMLInputElement>) => void;
+type DocumentNameKeyDownEventHandler = (event: KeyboardEvent<HTMLInputElement>) => void;
+
+interface DocumentHeaderNameProps extends ComponentProps<"div"> {
   onDocumentRename: (name: string) => void;
+}
+
+interface DocumentHeaderNameState {
+  draftName: string;
+  isRenaming: boolean;
 }
 
 export function DocumentHeaderName({
   onDocumentRename,
   className,
   ...props
-}: Props) {
+}: DocumentHeaderNameProps) {
   const initialDocument = useInitialDocument();
   const isReadOnly = useSelf((me) => !me.canWrite);
-  const [draftName, setDraftName] = useState(initialDocument.name);
-  const [isRenaming, setRenaming] = useState(false);
+  const [state, setState] = useState<DocumentHeaderNameState>({
+    draftName: initialDocument.name,
+    isRenaming: false,
+  });
 
-  const handleRenamingStart = useCallback(() => {
-    setRenaming(true);
+  const handleRenamingStart: DocumentRenameEventHandler = useCallback(() => {
+    setState((prevState) => ({ ...prevState, isRenaming: true }));
   }, []);
 
-  const handleRenamingCancel = useCallback(() => {
-    setDraftName(initialDocument.name);
-    setRenaming(false);
+  const handleRenamingCancel: DocumentRenameCancelEventHandler = useCallback(() => {
+    setState((prevState) => ({ ...prevState, draftName: initialDocument.name, isRenaming: false }));
   }, [initialDocument]);
 
-  const handleRenamingSave = useCallback(() => {
-    onDocumentRename(draftName);
-    setRenaming(false);
-  }, [draftName, onDocumentRename]);
+  const handleRenamingSave: DocumentRenameSaveEventHandler = useCallback(() => {
+    onDocumentRename(state.draftName);
+    setState((prevState) => ({ ...prevState, isRenaming: false }));
+  }, [state.draftName, onDocumentRename]);
 
-  const handleNameChange = useCallback(
+  const handleNameChange: DocumentNameChangeEventHandler = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
-      setDraftName(event.target.value);
+      setState((prevState) => ({ ...prevState, draftName: event.target.value }));
     },
     []
   );
 
-  const handleNameKeyDown = useCallback(
+  const handleNameKeyDown: DocumentNameKeyDownEventHandler = useCallback(
     (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key === "Enter") {
         handleRenamingSave();
@@ -57,6 +69,8 @@ export function DocumentHeaderName({
     },
     [handleRenamingCancel, handleRenamingSave]
   );
+
+  const { draftName, isRenaming } = state;
 
   return (
     <div className={clsx(className, styles.container)} {...props}>
@@ -89,4 +103,44 @@ export function DocumentHeaderName({
       )}
     </div>
   );
+}
+
+interface TooltipPropsWithChildren extends TooltipProps {
+  children: JSX.Element;
+}
+
+export function TooltipWithChildren({ children, ...props }: TooltipPropsWithChildren) {
+  return <Tooltip {...props}>{children}</Tooltip>;
+}
+
+interface ButtonPropsWithChildren extends ComponentProps<"button"> {
+  children: JSX.Element;
+}
+
+export function ButtonWithChildren({ children, ...props }: ButtonPropsWithChildren) {
+  return <button {...props}>{children}</button>;
+}
+
+interface InputPropsWithChildren extends ComponentProps<"input"> {
+  children: JSX.Element;
+}
+
+export function InputWithChildren({ children, ...props }: InputPropsWithChildren) {
+  return <input {...props}>{children}</input>;
+}
+
+interface SpanPropsWithChildren extends ComponentProps<"span"> {
+  children: JSX.Element;
+}
+
+export function SpanWithChildren({ children, ...props }: SpanPropsWithChildren) {
+  return <span {...props}>{children}</span>;
+}
+
+interface DivPropsWithChildren extends ComponentProps<"div"> {
+  children: JSX.Element;
+}
+
+export function DivWithChildren({ children, ...props }: DivPropsWithChildren) {
+  return <div {...props}>{children}</div>;
 }
