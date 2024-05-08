9aaa74c04fa20e6e6d8af4e0d89ef2c88a371a92
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:49 +0100

    improve

--- primitives/Button/index.ts
+++ primitives/Button/index.ts
@@ -1,1 +1,11 @@
-export * from "./Button";
+// Button.js
+export const ButtonPrimary = ({ children, ...props }) => {
+  // ButtonPrimary component implementation
+};
+
+export const ButtonSecondary = ({ children, ...props }) => {
+  // ButtonSecondary component implementation
+};
+
+// index.js
+export { ButtonPrimary, ButtonSecondary } from "./Button";
