343187751058810679ebcce5f67eaff95db607d7
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:01 +0100

    improve

--- icons/Delete.tsx
+++ icons/Delete.tsx
@@ -1,25 +1,34 @@
-import { ComponentProps } from "react";
+import { SVGProps } from "react";
 
-export function DeleteIcon(props: ComponentProps<"svg">) {
+export type DeleteIconProps = SVGProps<SVGSVGElement> & {
+  className?: string;
+  color?: string;
+  size?: number;
+};
+
+export function DeleteIcon(props: DeleteIconProps) {
+  const { className, color = "currentColor", size = 16, ...svgProps } = props;
+
   return (
     <svg
-      width="16"
-      height="16"
+      width={size}
+      height={size}
       viewBox="0 0 16 16"
+      className={className}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      {...svgProps}
     >
       <path
         d="M4 5h8l-.672 7.14a1.5 1.5 0 0 1-1.493 1.36h-3.67a1.5 1.5 0 0 1-1.493-1.36L4 5ZM5.5 3.5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1V5h-5V3.5Z"
-        stroke="currentColor"
+        stroke={color}
         strokeWidth="2"
         strokeLinejoin="round"
         vectorEffect="non-scaling-stroke"
       />
       <path
         d="M2.5 5h11"
-        stroke="currentColor"
+        stroke={color}
         strokeWidth="2"
         strokeLinecap="round"
         vectorEffect="non-scaling-stroke"
