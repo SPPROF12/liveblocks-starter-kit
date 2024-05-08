2b322bfe2a277132226eae840ba204a51307dcd7
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:13 +0100

    improve

--- app/dashboard/group/[groupId]/page.tsx
+++ app/dashboard/group/[groupId]/page.tsx
@@ -1,9 +1,17 @@
 import { DocumentsLayout } from "@/layouts/Documents";
 
-type Props = {
-  params: { groupId: string };
+type Params = {
+  groupId: string;
 };
 
-export default async function DashboardGroupPage({ params }: Props) {
-  return <DocumentsLayout filter="group" groupId={params.groupId} />;
+export async function getServerSideProps({ params }: { params: Params }) {
+  return {
+    props: {
+      groupId: params.groupId,
+    },
+  };
+}
+
+export default function DashboardGroupPage({ groupId }: Params) {
+  return <DocumentsLayout filter="group" groupId={groupId} />;
 }
