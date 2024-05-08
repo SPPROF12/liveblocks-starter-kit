b9495384af4cf793fcea08c3cdca8f5ccacc2c29
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:40 +0100

    improve

--- components/Inbox/index.ts
+++ components/Inbox/index.ts
@@ -1,2 +1,9 @@
-export * from "./Inbox";
-export * from "./InboxPopover";
+// index.ts
+
+export { default as Inbox } from "./Inbox";
+export { default as InboxPopover } from "./InboxPopover";
+
+// or if you want to re-export the entire module
+export * as InboxModule from "./Inbox";
+export * as InboxPopoverModule from "./InboxPopover";
+
