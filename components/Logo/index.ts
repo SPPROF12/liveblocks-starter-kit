fcc7a664d23c69f198b9bfe3349e40ed97fd5e18
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:28 +0100

    improve

--- components/Logo/index.ts
+++ components/Logo/index.ts
@@ -1,1 +1,4 @@
-export * from "./Logo";
+// re-export individual named exports from "./Logo" module
+export { default as Logo } from "./Logo";
+export { LogoVersion1, LogoVersion2 } from "./Logo";
+// add more named exports as needed
