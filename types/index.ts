a2ff654c9b136ad96f1c6617c636ca3ac0ebda1a
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:25 +0100

    improve

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,14 @@
-export * from "./data";
-export * from "./document";
+// data.ts
+export * from "./data/dataModule1";
+export * from "./data/dataModule2";
+
+// document.ts
+export * from "./document/documentModule1";
+export * from "./document/documentModule2";
+
+// index.ts
+import * as data from "./data";
+import * as document from "./document";
+
+// Add type declarations here if using TypeScript
+export { data, document };
