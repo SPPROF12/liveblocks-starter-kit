87f9cd1105cf956934ccb1db5e58215a1263f851
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:46 +0100

    improve

--- utils/capitalize.ts
+++ utils/capitalize.ts
@@ -1,3 +1,7 @@
-export function capitalize(string: string) {
+export const capitalize = (string: string): string => {
+  if (typeof string !== 'string') {
+    throw new Error('Input should be a string');
+  }
+
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
