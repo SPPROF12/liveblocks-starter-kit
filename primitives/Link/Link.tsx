6dc4522d0d05ca48e43c8851ad5a17eda5ec713b
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:49 +0100

    improve

--- primitives/Link/Link.tsx
+++ primitives/Link/Link.tsx
@@ -1,10 +1,21 @@
 import DefaultLink from "next/link";
-import { ComponentProps } from "react";
+import { ComponentPropsWithoutRef, ReactElement } from "react";
 
 interface LinkProps extends Omit<ComponentProps<typeof DefaultLink>, "href"> {
+  href: string;
+}
+
+export function Link({ href, ...props }: LinkProps): ReactElement {
+  return <DefaultLink href={href} {...props} />;
+}
+
+interface AnchorProps extends Omit<ComponentProps<"a">, "href"> {
   href?: string;
 }
 
-export function Link({ href, ...props }: LinkProps) {
-  return href ? <DefaultLink href={href} {...props} /> : <a {...props} />;
+export function Anchor({ href, ...props }: AnchorProps): ReactElement | null {
+  if (!href) {
+    return null;
+  }
+  return <a href={href} {...props} />;
 }
