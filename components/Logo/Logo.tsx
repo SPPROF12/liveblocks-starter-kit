2478c147368a50b90899daa480552d63a74b231c
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:50 +0100

    improve

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,7 +2,15 @@ import clsx from "clsx";
 import { ComponentProps } from "react";
 import styles from "./Logo.module.css";
 
-export function Logo({ className, ...props }: ComponentProps<"div">) {
+export type LogoProps = ComponentProps<"div"> & {
+  title?: string;
+};
+
+export function Logo({
+  className,
+  title,
+  ...props
+}: LogoProps): JSX.Element {
   return (
     <div className={clsx(className, styles.logo)} {...props}>
       <svg
@@ -10,7 +18,11 @@ export function Logo({ className, ...props }: ComponentProps<"div">) {
         fill="none"
         viewBox="0 0 32 32"
         xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-labelledby="logo-title logo-desc"
       >
+        <title id="logo-title">{title || "Starter Kit Logo"}</title>
+        <desc id="logo-desc">A stylized letter 'S' representing Starter Kit</desc>
         <path
           clipRule="evenodd"
           d="M21.657 8H2l5.657 5.6v7.733L21.657 8ZM10.343 24H30l-5.657-5.6v-7.733L10.343 24Z"
