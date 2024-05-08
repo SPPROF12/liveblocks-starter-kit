456ff1f0a1777eca4a2a3a1b954eda9deb38f1da
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:01:39 +0100

    improve

--- primitives/Link/index.ts
+++ primitives/Link/index.ts
@@ -1,1 +1,35 @@
-export * from "./Link";
+import * as LinkModule from "./Link";
+
+export { LinkModule };
+
+
+export class Link {
+  constructor(private url: string, private text: string) {}
+
+  getUrl(): string {
+    return this.url;
+  }
+
+  getText(): string {
+    return this.text;
+  }
+}
+
+
+declare module "./Link" {
+  export class Link {
+    constructor(url: string, text: string);
+    getUrl(): string;
+    getText(): string;
+  }
+}
+
+
+npm install my-link-module
+
+
+import { LinkModule } from "my-link-module";
+
+const link = new LinkModule.Link("https://www.example.com", "Example");
+console.log(link.getText()); // Example
+console.log(link.getUrl()); // https://www.example.com
