5732272e16b52ea19f5277f4ab501b24eb6862e3
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:41 +0100

    improve

--- lib/utils/isUserDocumentOwner.ts
+++ lib/utils/isUserDocumentOwner.ts
@@ -7,22 +7,20 @@ interface IsUserDocumentOwnerProps {
 }
 
 /**
- * Returns true if owner is set on room, and owner is equal to userId
+ * Returns true if the owner of the room is equal to the given user id
  *
  * @param room - The room to test
  * @param userId - The user's id to check
  */
-export function isUserDocumentOwner({
-  room,
-  userId,
-}: IsUserDocumentOwnerProps): boolean {
-  if (!room?.metadata?.owner) {
+export function isUserDocumentOwner({ room, userId }: IsUserDocumentOwnerProps): boolean {
+  // Return false if room or owner is not defined
+  if (!room || !room.metadata || !room.metadata.owner) {
     return false;
   }
 
-  const owner = Array.isArray(room.metadata.owner)
-    ? room.metadata.owner[0]
-    : room.metadata.owner;
+  // Get the first owner if owner is an array
+  const owner = Array.isArray(room.metadata.owner) ? room.metadata.owner[0] : room.metadata.owner;
 
+  // Check if the owner is equal to the user id
   return owner === userId;
 }
