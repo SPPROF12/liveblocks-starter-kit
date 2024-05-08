5c3b150bed508e6ba477d223f728e5f765d97343
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-09 00:02:14 +0100

    improve

--- primitives/Spinner/index.ts
+++ primitives/Spinner/index.ts
@@ -1,1 +1,4 @@
-export * from "./Spinner";
+// re-export all named exports from "./Spinner" module
+export { default as Spinner } from "./Spinner";
+export { SpinnerProps } from "./Spinner";
+// include any other named exports from "./Spinner" here
